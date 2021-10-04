ad6b40bccdb73953a39e3b35d9076e0ce6700f5b
dimitermusic <dimiteryyordanov@gmail.com>
2021-10-03 21:57:19 -0700

    js almost complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
-// Confirm js file is properly linked
-console.log("js linked");
 var storeUserInput = document.querySelector(".user-search").value;
 var userHistory = []
 var searchBtn = document.querySelector(".btn");
 var displayHistory = document.querySelector(".search-history")
-// var newApi = "api.openweathermap.org/data/2.5/weather?q=" + storeUserInput + "&appid=605deb5c1cc4ba7a3d01c08035503f22";
+var customApi = ""
 
 function handlingBtn() {
     var newSearch = document.querySelector(".user-search").value;
     userHistory.push(newSearch)
     localStorage.setItem("user-history", userHistory)
     console.log(userHistory);
-    // Add custom url based on user input.
-    // Create conditional statement to check that city is valid by checking that the url works, if not alert "Please choose a valid city!"
     displayUserHistory();
 }
 
@@ -23,19 +19,26 @@ function displayUserHistory() {
         createSearchHistory.innerHTML = userHistory[i];
         createSearchHistory.setAttribute("class", "btn btn-secondary col-12 m-2");
         displayHistory.append(createSearchHistory);
+
+        var newApi = "https://api.openweathermap.org/data/2.5/weather?q=" + userHistory[i] + "&appid=605deb5c1cc4ba7a3d01c08035503f22";
+        console.log(newApi);
+        customApi = newApi
+        parseApi();
     }
 }
 
 searchBtn.addEventListener("click", handlingBtn);
 
-// function parseApi() {
-//     fetch("api.openweathermap.org/data/2.5/weather?q=seattle&appid=605deb5c1cc4ba7a3d01c08035503f22")
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         });
-// }
-
-// parseApi();
+function parseApi() {
+    fetch(customApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+        data.
+        if (response.status === 200) {
+            
+        }
+}
